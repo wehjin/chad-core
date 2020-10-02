d1743ae63c56eaa0ff9236ff3e7bb9d589908b3c
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-10-01 21:02:27 -0700

    Adds drift_reports() to Squad.

--- src/core.rs
+++ src/core.rs
@@ -12,7 +12,7 @@ pub struct Squad {
 }
 
 impl Squad {
-	pub fn shares(&self) -> HashMap<String, f64> {
+	pub fn symbol_shares(&self) -> HashMap<String, f64> {
 		let mut hashmap = HashMap::new();
 		for lot in &self.lots {
 			let previous = *hashmap.get(&lot.symbol).unwrap_or(&0.0);
@@ -20,6 +20,43 @@ impl Squad {
 			hashmap.insert(lot.symbol.to_owned(), next);
 		}
 		hashmap
+	}
+	pub fn member_portions(&self) -> Vec<f64> {
+		let mut sum_weights = 0.0;
+		let mut weights = Vec::new();
+		for index in 0..self.members.len() {
+			let rank = (index + 1) as f64;
+			let weight = rank * rank;
+			weights.push(weight);
+			sum_weights += weight;
+		};
+		weights.into_iter().map(|it| {
+			if sum_weights > 0.0 { it / sum_weights } else { 0.0 }
+		}).collect()
+	}
+	fn member_market_values(&self) -> Vec<f64> {
+		let symbol_shares = self.symbol_shares();
+		self.members.iter().map(|it| it.market_value(&symbol_shares)).collect()
+	}
+	pub fn drift_reports(&self) -> Vec<DriftReport> {
+		let portions = self.member_portions();
+		let market_values = self.member_market_values();
+		let sum_targets = market_values.iter().sum::<f64>() + self.unspent;
+		let target_values = portions.iter().map(|portion| {
+			match sum_targets > 0.0 {
+				true => *portion * sum_targets,
+				false => 0.0,
+			}
+		}).collect::<Vec<_>>();
+		self.members.clone().into_iter().enumerate().map(|(index, member)| {
+			DriftReport {
+				member,
+				rank: index + 1,
+				target_portion: portions[index],
+				market_value: market_values[index],
+				target_value: target_values[index],
+			}
+		}).collect()
 	}
 }
 
@@ -30,6 +67,13 @@ pub struct SquadMember {
 	pub price: f64,
 }
 
+impl SquadMember {
+	pub fn market_value(&self, symbol_shares: &HashMap<String, f64>) -> f64 {
+		let shares = *symbol_shares.get(&self.symbol).unwrap_or(&0.0);
+		self.price * shares
+	}
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Lot {
 	pub squad_id: u64,
@@ -37,4 +81,28 @@ pub struct Lot {
 	pub symbol: String,
 	pub account: String,
 	pub shares: f64,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DriftReport {
+	pub member: SquadMember,
+	pub rank: usize,
+	pub market_value: f64,
+	pub target_portion: f64,
+	pub target_value: f64,
+}
+
+impl DriftReport {
+	pub fn symbol(&self) -> &str {
+		&self.member.symbol
+	}
+	pub fn drift_amount(&self) -> f64 {
+		self.market_value - self.target_value
+	}
+	pub fn drift_shares(&self) -> Option<f64> {
+		match self.member.price >= 0.0 {
+			true => Some(self.drift_amount() / self.member.price),
+			false => None
+		}
+	}
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,12 +1,13 @@
 use chad_core::chad::Chad;
-use chad_core::core::{Lot, SquadMember};
+use chad_core::core::{DriftReport, Lot, SquadMember};
 
 const OWNER: u64 = 1000;
 const SQUAD_ID: u64 = 2000;
 const SQUAD_NAME: &str = "Blue";
 const SYMBOL1: &str = "KO";
 const SYMBOL2: &str = "PEP";
-const LOT_ID: u64 = 3000;
+const LOT_ID1: u64 = 3000;
+const LOT_ID2: u64 = 3001;
 const ACCOUNT1: &str = "main";
 const SHARES1: f64 = 10.0;
 const PRICE1: f64 = 5.0;
@@ -63,13 +64,13 @@ fn add_lot_registers_a_lot() {
 	let chad = Chad::connect_tmp();
 	chad.add_squad(SQUAD_ID, SQUAD_NAME, OWNER);
 	chad.add_member(SQUAD_ID, SYMBOL1, PRICE1);
-	chad.add_lot(SQUAD_ID, LOT_ID, SYMBOL1, ACCOUNT1, SHARES1);
+	chad.add_lot(SQUAD_ID, LOT_ID1, SYMBOL1, ACCOUNT1, SHARES1);
 	let squads = chad.snap().squads(OWNER);
 	let squad = squads.first().expect("First squad");
 	let lots = squad.lots.first().expect("First lot");
 	assert_eq!(&Lot {
 		squad_id: SQUAD_ID,
-		id: LOT_ID,
+		id: LOT_ID1,
 		symbol: SYMBOL1.to_string(),
 		account: ACCOUNT1.to_string(),
 		shares: SHARES1,
@@ -81,9 +82,48 @@ fn squad_shares_match_inputs() {
 	let chad = Chad::connect_tmp();
 	chad.add_squad(SQUAD_ID, SQUAD_NAME, OWNER);
 	chad.add_member(SQUAD_ID, SYMBOL1, PRICE1);
-	chad.add_lot(SQUAD_ID, LOT_ID, SYMBOL1, ACCOUNT1, SHARES1);
+	chad.add_lot(SQUAD_ID, LOT_ID1, SYMBOL1, ACCOUNT1, SHARES1);
 	let squads = chad.snap().squads(OWNER);
 	let squad = squads.first().expect("First squad");
-	let shares = squad.shares();
+	let shares = squad.symbol_shares();
 	assert_eq!(SHARES1, shares[SYMBOL1])
+}
+
+#[test]
+fn squad_produces_drift_reports() {
+	let chad = Chad::connect_tmp();
+	chad.add_squad(SQUAD_ID, SQUAD_NAME, OWNER);
+	chad.add_member(SQUAD_ID, SYMBOL1, 1.0);
+	chad.add_lot(SQUAD_ID, LOT_ID1, SYMBOL1, ACCOUNT1, 3.0);
+	chad.add_member(SQUAD_ID, SYMBOL2, 1.0);
+	chad.add_lot(SQUAD_ID, LOT_ID2, SYMBOL2, ACCOUNT1, 2.0);
+	let squads = chad.snap().squads(OWNER);
+	let squad = squads.first().expect("First squad");
+	let reports = squad.drift_reports();
+	let drifts_shares = reports.iter().map(|it| (it.drift_amount(), it.drift_shares())).collect::<Vec<_>>();
+	assert_eq!(vec![(1.0, Some(1.0)), (-1.0, Some(-1.0))], drifts_shares);
+	assert_eq!(vec![
+		DriftReport {
+			member: SquadMember {
+				squad_id: SQUAD_ID,
+				symbol: SYMBOL2.to_string(),
+				price: 1.0,
+			},
+			rank: 1,
+			market_value: 2.0,
+			target_portion: 0.2,
+			target_value: 1.0,
+		},
+		DriftReport {
+			member: SquadMember {
+				squad_id: SQUAD_ID,
+				symbol: SYMBOL1.to_string(),
+				price: 1.0,
+			},
+			rank: 2,
+			market_value: 3.0,
+			target_portion: 0.8,
+			target_value: 4.0,
+		}
+	], reports);
 }
